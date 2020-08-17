cafbf4d74e9f8d947a7190af6d2ab17c37918418
j1000tonm <j1000tonm.jmb@gmail.com>
2020-08-16 21:22:37 -0300

    MainView Controller

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,94 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController implements Initializable {
+
+	@FXML
+	private MenuItem menuItemVendedor;
+	
+	@FXML
+	private MenuItem menuItemFornecedor;
+	
+	@FXML
+	private MenuItem menuItemCliente;
+	
+	@FXML
+	private MenuItem menuItemMarca;
+	
+	@FXML
+	private MenuItem menuItemSetor;
+	
+	@FXML
+	private MenuItem menuItemSubSetor;
+	
+	@FXML
+	private MenuItem menuItemCategoria;
+	
+	@FXML
+	private MenuItem menuItemSubCategoria;
+	
+	@FXML
+	private MenuItem menuItemProduto;
+	
+	@FXML
+	private MenuItem menuItemServico;
+	
+	@FXML
+	private MenuItem menuItemAbout;
+	
+	
+	@FXML
+	public void onMenuItemVendedor() {		
+	}
+	
+	@FXML
+	public void onMenuItemFonercedor() {		
+	}
+	
+	@FXML
+	public void onMenuItemCliente() {		
+	}
+	
+	@FXML
+	public void onMenuItemMarca() {		
+	}
+	
+	@FXML
+	public void onMenuItemSetor() {		
+	}
+	
+	@FXML
+	public void onMenuItemSubSetor() {		
+	}
+	
+	@FXML
+	public void onMenuItemCategoria() {		
+	}
+	
+	@FXML
+	public void onMenuItemSubCategoria() {		
+	}
+	
+	@FXML
+	public void onMenuItemProduto() {		
+	}
+	
+	@FXML
+	public void onMenuItemServico() {		
+	}
+	
+	@FXML
+	public void onMenuItemAbout() {		
+	}
+	
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+	}
+
+}
