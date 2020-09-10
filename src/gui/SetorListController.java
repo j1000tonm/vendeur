cc63a145256d0a50821d8415871d2473a5bebedd
j1000tonm <j1000tonm.jmb@gmail.com>
2020-09-09 23:21:29 -0300

    Atualizando e deletando setor

--- src/gui/SetorListController.java
+++ src/gui/SetorListController.java
@@ -3,12 +3,15 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 import application.Main;
+import db.DbIntegrityException;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -18,6 +21,8 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -39,6 +44,12 @@ public class SetorListController implements Initializable, DataChangeListener {
 	
 	@FXML
 	private TableColumn<Setor, String> tableColumnNome;
+	
+	@FXML
+	private TableColumn<Setor, Setor> tableColumnEDIT;
+	
+	@FXML
+	private TableColumn<Setor, Setor> tableColumnREMOVE;
 	
 	@FXML
 	private Button btNovoSetor;
@@ -76,6 +87,8 @@ public class SetorListController implements Initializable, DataChangeListener {
 		List<Setor> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewSetor.setItems(obsList);
+		initEditButtons();
+		initRemoveButtons();
 	}
 	
 	private void createDialogForm(Setor obj, String absoluteName, Stage parentStage) {
@@ -105,5 +118,65 @@ public class SetorListController implements Initializable, DataChangeListener {
 	@Override
 	public void onDataChanged() {
 		updateTableView();
+	}
+	
+	private void initEditButtons() {
+		tableColumnEDIT.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEDIT.setCellFactory(param -> new TableCell<Setor, Setor>() {
+			private final Button button = new Button("edit");
+			
+			@Override
+			protected void updateItem(Setor obj, boolean empty) {
+				super.updateItem(obj, empty);
+				
+				if (obj == null) {
+						setGraphic(null);
+						return;
+				}
+				
+				setGraphic(button);
+				button.setOnAction(
+				event -> createDialogForm(
+						obj, "/gui/SetorForm.fxml",Utils.currentStage(event)));
+						
+			}
+		});
+	}
+	
+	private void initRemoveButtons() {
+		tableColumnREMOVE.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnREMOVE.setCellFactory(param -> new TableCell<Setor, Setor>() {
+			private final Button button = new Button("remove");
+			
+			@Override
+			protected void updateItem(Setor obj, boolean empty) {
+				super.updateItem(obj, empty);
+				
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				
+				setGraphic(button);
+				button.setOnAction(event -> removeEntity(obj));
+			}
+		});
+	}
+
+	private void removeEntity(Setor obj) {
+		Optional<ButtonType> result = Alerts.showConfirmation("Confirmação", "Você tem certeza que quer deletar?");
+		
+		if (result.get() == ButtonType.OK) {
+			if (service == null) {
+				throw new IllegalStateException("Serviço está nulo");
+			}
+			try {
+				service.remove(obj);
+				updateTableView();				
+			}
+			catch (DbIntegrityException e) {
+				Alerts.showAlert("Erro removendo objeto", null, e.getMessage(), AlertType.ERROR);
+			}
+		}
 	}
 }
